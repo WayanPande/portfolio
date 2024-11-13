6a5ddbd8ecefd1daba24bf8ad34b192a5a9cf2ad
WayanPande <yande554@gmail.com>
2024-11-13 23:34:41 +0800

    feat: update control command

--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -21,6 +21,10 @@ interface Props {
 export const CommandMenu = ({ links }: Props) => {
   const [open, setOpen] = React.useState(false);
 
+  const isMac =
+    typeof window !== "undefined" &&
+    /Mac|iPod|iPhone|iPad/.test(window.navigator?.userAgent || "");
+
   React.useEffect(() => {
     const down = (e: KeyboardEvent) => {
       if (e.key === "j" && (e.metaKey || e.ctrlKey)) {
@@ -35,10 +39,11 @@ export const CommandMenu = ({ links }: Props) => {
 
   return (
     <>
-      <p className="fixed bottom-0 left-0 right-0 hidden border-t border-t-muted bg-white p-1 text-center text-sm text-muted-foreground print:hidden xl:block">
+      <p className="fixed bottom-0 left-0 right-0 hidden border-t border-t-muted bg-white p-1 text-center text-sm text-muted-foreground xl:block print:hidden">
         Press{" "}
         <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-          <span className="text-xs">⌘</span>J
+          <span className={isMac ? "text-xs" : "hidden"}>⌘ J</span>
+          <span className={!isMac ? "text-xs" : "hidden"}>Ctrl J</span>
         </kbd>{" "}
         to open the command menu
       </p>
@@ -46,7 +51,7 @@ export const CommandMenu = ({ links }: Props) => {
         onClick={() => setOpen((open) => !open)}
         variant="outline"
         size="icon"
-        className="fixed bottom-4 right-4 flex rounded-full shadow-2xl print:hidden xl:hidden"
+        className="fixed bottom-4 right-4 flex rounded-full shadow-2xl xl:hidden print:hidden"
       >
         <CommandIcon className="my-6 size-6" />
       </Button>
