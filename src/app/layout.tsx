4f804b8c296314237520fbcf42dffc410b2bcf6a
WayanPande <yande554@gmail.com>
2026-02-04 14:10:12 +0700

    feat: integrate PostHog for analytics tracking and add environment variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import React from "react";
 import Navbar from "@/components/Navbar";
+import { PHProvider } from './providers';
 
 export const metadata: Metadata = {
   robots: "all",
@@ -20,7 +21,7 @@ export default function RootLayout({
         <Navbar />
         <main className="bg-grid-small-black/20 dark:bg-grid-small-white/20">
           <div className="pointer-events-none fixed inset-0 flex items-center justify-center bg-white mask-[radial-gradient(ellipse_at_center,transparent_60%,black)] dark:bg-black"></div>
-          {children}
+          <PHProvider>{children}</PHProvider>
         </main>
       </body>
     </html>
---
+++ src/app/providers.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+if (typeof window !== "undefined" && process.env.NODE_ENV !== "development") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
+    // Optional: Disable automatic pageview capture if you want to manually track page views
+    // capture_pageview: false,
+  });
+}
+
+export function PHProvider({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (pathname && posthog._loaded()) {
+      posthog.capture("$pageview", {
+        $current_url: pathname + searchParams.toString(),
+      });
+    }
+  }, [pathname, searchParams]);
+
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+}
